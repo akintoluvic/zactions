88ccc20cc570d84f916759da1c7c191addc00d5d
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-02-28 15:09:30 +0100

    routes set

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { getTransactions, addTransactions, deleteTransactions } = require('../contollers/transaction')
 
-router.get('/', (req, res) => res.send('Hello'))
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransactions)
+    
+
+router
+    .route('/:id')
+    .delete(deleteTransactions)
 
 module.exports = router;
