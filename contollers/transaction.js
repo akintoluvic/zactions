af7f6aa0aaab80f91340317a5e1a57f8adee44af
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-02-28 16:08:08 +0100

    error + validation

--- contollers/transaction.js
+++ contollers/transaction.js
@@ -33,7 +33,18 @@ exports.addTransactions = async (req, res, next) => {
             data: transaction
         })
     } catch (error) {
-        console.log(error)
+        if(error.name === 'ValidationError') {
+            const messages = Object.values(error.errors).map(val => val.message);
+            res.status(400).json({
+                success: false,
+                error: messages
+            })
+        } else {
+            return res.send(500).json({
+                success: false,
+                error: 'Server Error'
+            })
+        }
     }
 }
 
