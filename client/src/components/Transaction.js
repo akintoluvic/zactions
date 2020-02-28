5a9e19aed1e4ce4fd7a446eb3d9704e10f21bf9b
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-02-28 18:33:56 +0100

    format nos with commas

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { numberWithCommas } from '../utils/format';
 
 export const Transaction = ({ transaction }) => {
     const { deleteTransaction } = useContext(GlobalContext);
@@ -8,7 +9,7 @@ export const Transaction = ({ transaction }) => {
     return (
         <li className={transaction.amount < 0 ? "minus" : "plus"}> 
             {transaction.text} 
-            <span>{sign}${Math.abs(transaction.amount)}</span>
+            <span>{sign}${numberWithCommas(Math.abs(transaction.amount))}</span>
             <button 
                 className="delete-btn"
                 onClick={() => deleteTransaction(transaction._id)}
